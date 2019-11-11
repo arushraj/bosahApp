153f857bdd2b68bd66879fc48a860aa77f139f09
arushraj <arushraj7@gmail.com>
2019-11-11 22:38:28 +0530

    update on 11-11-2019
    update on 11-11-2019

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -22,7 +22,7 @@ export class UserRegistrationComponent implements OnInit {
 
   public otpsend = false;
   private otp;
-  private isValid = true;
+  private isValid = false;
   public displayImage = '/assets/no-image.png';
   public newUser = {
     email: '',
